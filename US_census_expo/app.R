373e6deea8e2cd5ca4ea60684f7d136623df5578
Beau Meche <beau_meche@college.harvard.edu>
2019-04-30 07:16:45 -0400

    ICE push, have two working tabs now

--- US_census_expo/app.R
+++ US_census_expo/app.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(tidyverse)
+library(tigris)
 library(stringr)
 library(janitor)
 library(gt)
@@ -7,8 +7,12 @@ library(dplyr)
 library(ggthemes)
 library(ggplot2)
 library(leaflet)
+library(tidyverse)
 
 youth_2017 <- read_rds("young_people_locations")
+agg_total_pop_2 <- read_rds("aggregate_pops")
+states <- states()
+all_us <- geo_join(states, agg_total_pop, "NAME", "geography")
 
 # Define UI for application
 ui <- fluidPage(
@@ -20,7 +24,12 @@ ui <- fluidPage(
       mainPanel(
         tabsetPanel(
           tabPanel("Young People",
-                   plotOutput("youth")))
+                   plotOutput("youth")),
+          tabPanel("Census Totals '17",
+                   leafletOptions("pop_17")),
+          tabPanel("Population Flux",
+                   leafletOutput("big_map"))
+        )
       )
    )
 
@@ -38,7 +47,83 @@ server <- function(input, output) {
          caption = "Source: the U.S. Census")
    })
    
-   output$big__map <- renderLeaflet()
+   output$big_map <- renderLeaflet({
+     
+     bins <- c(-45000, -15000, 0, 15000, 30000,
+               45000, 100000, 200000, Inf)
+     
+     pal <- colorBin("YlOrRd", domain = all_us$change, bins = bins)
+     
+     labels <- sprintf(
+       "<strong>%s</strong><br/>%g people",
+       all_us$geography, all_us$change
+     ) %>% lapply(htmltools::HTML)
+     
+     leaflet(all_us) %>%
+       setView(-96, 37.8, 3) %>%
+       addProviderTiles("MapBox", options = providerTileOptions(
+         id = "mapbox.light",
+         accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+       addPolygons(
+         fillColor = ~pal(change),
+         weight = 2,
+         opacity = 1,
+         color = "white",
+         dashArray = "3",
+         fillOpacity = 0.7,
+         highlight = highlightOptions(
+           weight = 5,
+           color = "#750",
+           dashArray = "",
+           fillOpacity = 0.7,
+           bringToFront = TRUE),
+         label = labels,
+         labelOptions = labelOptions(
+           style = list("font-weight" = "normal", padding = "3px 8px"),
+           textsize = "15px",
+           direction = "auto")) %>%
+       addLegend(pal = pal, values = ~all_us$change, opacity = 0.7, title = NULL,
+                 position = "bottomright")
+   })
+   
+   output$pop_17 <- renderLeaflet({
+     
+     # bins <- c(-45000, -15000, 0, 15000, 30000,
+     #           45000, 100000, 200000, Inf)
+     
+     pal <- colorBin("YlOrRd", domain = all_us$totalpop.17)
+     
+     labels <- sprintf(
+       "<strong>%s</strong><br/>%g people",
+       all_us$geography, all_us$totalpop.17
+     ) %>% lapply(htmltools::HTML)
+     
+     leaflet(all_us) %>%
+       setView(-96, 37.8, 3) %>%
+       addProviderTiles("MapBox", options = providerTileOptions(
+         id = "mapbox.light",
+         accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+       addPolygons(
+         fillColor = ~pal(totalpop.17),
+         weight = 2,
+         opacity = 1,
+         color = "white",
+         dashArray = "3",
+         fillOpacity = 0.7,
+         highlight = highlightOptions(
+           weight = 5,
+           color = "#750",
+           dashArray = "",
+           fillOpacity = 0.7,
+           bringToFront = TRUE),
+         label = labels,
+         labelOptions = labelOptions(
+           style = list("font-weight" = "normal", padding = "3px 8px"),
+           textsize = "15px",
+           direction = "auto")) %>%
+       addLegend(pal = pal, values = ~all_us$totalpop.17, opacity = 0.7, title = NULL,
+                 position = "bottomright")
+   })
 }
 
 # Run the application 
--- script.R
+++ script.R
@@ -1,10 +1,11 @@
-library(tidyverse)
 library(stringr)
 library(janitor)
 library(gt)
 library(dplyr)
 library(ggthemes)
 library(ggplot2)
+library(tigris)
+library(tidyverse)
 
 download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Census_2017_edu_attainment.csv",
               destfile = "2017_data.csv",
@@ -44,10 +45,76 @@ young_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pat
         subtitle = "Shown: the 10 states with the most people aged 18 to 24", 
         caption = "Source: the U.S. Census")
 
-
-total_pop_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total_estimate_population")|
-                                         str_detect(names(census_2017_nomargin), pattern = "geography")] %>%
-  mutate(totalpop = total_estimate_population_18_to_24_years + total_estimate_population_25_to_34_years +
-           total_estimate_population_35_to_44_years + total_estimate_population_45_to_64_years +
-           total_estimate_population_65_years_and_over) %>%
-  select(geography, totalpop)
+# total populations and  aggregate table creation
+      
+  total_pop_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total_estimate_population")|
+                                           str_detect(names(census_2017_nomargin), pattern = "geography")] %>%
+    mutate(totalpop = total_estimate_population_18_to_24_years + total_estimate_population_25_to_34_years +
+             total_estimate_population_35_to_44_years + total_estimate_population_45_to_64_years +
+             total_estimate_population_65_years_and_over) %>%
+    select(geography, totalpop)
+  
+  
+  total_pop_2016 <- census_2016_nomargin[, str_detect(names(census_2016_nomargin), pattern = "total_estimate_population")|
+                                           str_detect(names(census_2016_nomargin), pattern = "geography")] %>%
+    mutate(totalpop = total_estimate_population_18_to_24_years + total_estimate_population_25_to_34_years +
+             total_estimate_population_35_to_44_years + total_estimate_population_45_to_64_years +
+             total_estimate_population_65_years_and_over) %>%
+    select(geography, totalpop)
+  
+  # this data shows population flux by state from 2016-2017. Percentage change is
+  # negligible (ie. .22%), so whole #s seems preferable here
+  
+  agg_total_pop <- left_join(by = "geography", total_pop_2017, total_pop_2016,
+                             suffix = c(".17", ".16")) %>% 
+    mutate(sum = totalpop.17 + totalpop.16) %>% select(-sum) %>% 
+    mutate(change = ((totalpop.17 - totalpop.16))) %>% 
+    write_rds("aggregate_pop") %>% 
+    write_rds("aggregate_pops")
+  
+  
+# create the interactive plot for the app
+  
+  states <- states()
+  all_us <- geo_join(states, agg_total_pop, "NAME", "geography")
+  
+  bins <- c(-45000, -15000, 0, 15000, 30000,
+            45000, 100000, 200000, Inf)
+  
+  pal <- colorBin("YlOrRd", domain = all_us$change, bins = bins)
+  
+  labels <- sprintf(
+    "<strong>%s</strong><br/>%g people",
+    all_us$geography, all_us$change
+  ) %>% lapply(htmltools::HTML)
+  
+  leaflet(all_us) %>%
+    setView(-96, 37.8, 4) %>%
+    addProviderTiles("MapBox", options = providerTileOptions(
+      id = "mapbox.light",
+      accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+    addPolygons(
+      fillColor = ~pal(change),
+      weight = 2,
+      opacity = 1,
+      color = "white",
+      dashArray = "3",
+      fillOpacity = 0.7,
+      highlight = highlightOptions(
+        weight = 5,
+        color = "#750",
+        dashArray = "",
+        fillOpacity = 0.7,
+        bringToFront = TRUE),
+      label = labels,
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 8px"),
+        textsize = "15px",
+        direction = "auto")) %>%
+    addLegend(pal = pal, values = ~all_us$change, opacity = 0.7, title = NULL,
+              position = "bottomright")
+  
+  
+  
+  
+  
